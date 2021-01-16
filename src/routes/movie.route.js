5798c9e0dc24aafed3db741b23c28a1ab1ef6c95
HoaiBao <rorolo2011@gmail.com>
2021-01-16 19:38:10 +0700

    update;

--- src/routes/movie.route.js
+++ src/routes/movie.route.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import movieController from 'controllers/movie.controller';
-import groupMovieController from 'controllers/groupMovie.controller';
+import groupCinemaController from 'controllers/groupCinema.controller';
 import auth from 'middlewares/auth';
 const router = express.Router();
 
 router.get('/', movieController.getAll);
-router.get('/group', groupMovieController.getAll);
+router.get('/group', groupCinemaController.getAll);
 router.get('/:id', movieController.getMovieById);
 router.get('/reviews/:id', movieController.getMovieReviewsById);
 router.post('/reviews', auth(), movieController.createMovieReview);
