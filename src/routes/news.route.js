c06b58f5c550e28b902b0bac8787ef25c6833b4f
cuongcs4 <cn82295@gmail.com>
2021-01-23 01:27:18 +0700

    Add new news admin

--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import newsController from 'controllers/news.controller';
+import auth from 'middlewares/auth';
 const router = express.Router();
 
 router.get('/:id', newsController.getNewsById);
 router.get('/', newsController.getManyNews);
+router.post('/', auth(), newsController.createOrUpdateNews);
 
 export default router;
