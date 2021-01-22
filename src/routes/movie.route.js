3ce6b810b0a40adbdbf290b7222fbb40ba1e3847
ADMIN\Admin <thachdau16t@gmail.com>
2021-01-22 21:18:21 +0700

    fix them phim

--- src/routes/movie.route.js
+++ src/routes/movie.route.js
@@ -5,13 +5,13 @@ import auth from 'middlewares/auth';
 const router = express.Router();
 
 router.get('/', movieController.getAll);
+router.post('/', movieController.createMovie);
 router.get('/all', movieController.getAllMovie);
 router.get('/group', groupCinemaController.getAll);
 router.post('/group', groupCinemaController.getAll);
 router.post('/search', movieController.searchMovies);
 router.post('/reviews', auth(), movieController.createMovieReview);
 router.get('/:id', movieController.getMovieById);
-router.post('/:id', movieController.createMovie);
 router.put('/:id', movieController.updateMovieById);
 router.delete('/:id', movieController.deleteMovie);
 router.get('/reviews/:id', movieController.getMovieReviewsById);
